3e15f17777d166188a938fe1bd43f329b2fe249f
diariomuralcurico <diariomuralcurico@gmail.com>
2025-04-10 14:50:10 -0400

    Actualizacion para build

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -1,10 +1,8 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from 'firebase/storage';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
