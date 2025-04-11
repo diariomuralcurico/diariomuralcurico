ec2c26d73be69c76d2350afea2ea49cdd6fd9b8a
diariomuralcurico <diariomuralcurico@gmail.com>
2025-04-11 14:17:31 -0400

    Cambios en parrafos

--- src/pages/Inicio/Inicio.jsx
+++ src/pages/Inicio/Inicio.jsx
@@ -12,31 +12,31 @@ const Inicio = () => {
   return (
     <div className="p-4">
       <div className="">
-          <div >
-            <Carousel activeIndex={index} onSelect={handleSelect}>
-              <Carousel.Item>
-                <img
-                  className="carousel-img d-block w-100"
-                  src={require("./../../images/afcorpo.png")}
-                  alt="corporacion cultural de curico"
-                />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="carousel-img d-block w-100"
-                  src={require("./../../images/afmaca.jpeg")}
-                  alt="terapia gestalt curico"
-                />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  className="carousel-img d-block w-100"
-                  src={require("./../../images/afnagata.jpeg")}
-                  alt="nagata juegos"
-                />
-              </Carousel.Item>
-            </Carousel>
-          </div>
+        <div>
+          <Carousel activeIndex={index} onSelect={handleSelect}>
+            <Carousel.Item>
+              <img
+                className="carousel-img d-block w-100"
+                src={require("./../../images/afcorpo.png")}
+                alt="corporacion cultural de curico"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="carousel-img d-block w-100"
+                src={require("./../../images/afmaca.jpeg")}
+                alt="terapia gestalt curico"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="carousel-img d-block w-100"
+                src={require("./../../images/afnagata.jpeg")}
+                alt="nagata juegos"
+              />
+            </Carousel.Item>
+          </Carousel>
+        </div>
       </div>
       <div className="container my-2">
         <Row className="row d-flex justify-content-center align-items-center my-2">
@@ -49,25 +49,32 @@ const Inicio = () => {
               />
             </Ratio>
           </Col>
-          <Col md={12} lg={6} className="">
+          <Col md={12} lg={6}>
             <h1 className="text-center m-2 p-2 fw-bold mx-auto">
               Descripción General
             </h1>
-            <p className="text-center mx-auto m-2 p-2 fs-5">
+            <p className=" mx-auto fs-5 mb-2">
               En un mar de personas con banderas en mano, cada uno pretende
               visibilizar la propia levantándola por sobre las demás, sin mucho
-              éxito debido a que se pierden en la masa. Esta metáfora, refleja
-              el problema de dispersión de la agenda de actividades en la
-              provincia, debido a su publicación independiente a través de redes
-              sociales, donde su algoritmo no logra entregar una cartelera total
-              y congruente al público. Por otro lado, hemos observado que
-              nuestra gente manifiesta no estar enterada de las actividades que
-              se realizan en sus localidades y Diario Mural Curicó pretende dar
-              una solución focalizada a nivel provincial, aunando y
-              visibilizando una cartelera unificada de actividades para la
-              provincia. Así, queremos aportar a los gestores de actividades y a
-              quienes las buscan. Promoviendo espacios de encuentro, la
-              identidad cultural y una provincia más activa y mejor conectada.
+              éxito debido a que se pierden en la masa.
+            </p>
+            <p className=" mx-auto fs-5 mb-2">
+              Esta metáfora refleja el problema de dispersión de la agenda de
+              actividades en la provincia, debido a su publicación independiente
+              a través de redes sociales, donde su algoritmo no logra entregar
+              una cartelera total y congruente al público.
+            </p>
+            <p className=" mx-auto fs-5 mb-2">
+              Por otro lado, hemos observado que nuestra gente manifiesta no
+              estar enterada de las actividades que se realizan en sus
+              localidades y Diario Mural Curicó pretende dar una solución
+              focalizada a nivel provincial, aunando y visibilizando una
+              cartelera unificada de actividades para la provincia.
+            </p>
+            <p className=" mx-auto fs-5 mb-2">
+              Así, queremos aportar a los gestores de actividades y a quienes
+              las buscan. Promoviendo espacios de encuentro, la identidad
+              cultural y una provincia más activa y mejor conectada.
             </p>
           </Col>
         </Row>
@@ -75,20 +82,21 @@ const Inicio = () => {
       <div className="container">
         <Row className="row d-flex justify-content-center align-items-center mb-4 mt-4">
           <Col md={12} lg={6}>
-            <h1 className="text-center m-2 p-2 fw-bold mx-auto">
-              Quienes somos
-            </h1>
-            <p className="text-center m-2 p-2 fs-5 ">
+            <h1 className="text-center fw-bold mx-auto">Quienes somos</h1>
+            <p className="fs-5 mt-4 ">
               Somos vecinos de Curicó y esto es un proyecto de participación
-              ciudadana. Nos preocupa mucho la falta de participación de las
-              personas en la oferta cultural local y la generación de identidad
-              local, disfrutamos de nuestra bella ciudad y nos propusimos
-              desarrollar una solución a estas cuestiones que nos preocupan y
-              motivan mucho. ¡Por un Curicó, más vivo!
+              ciudadana.
             </p>
+            <p className="fs-5 ">
+              Nos preocupa mucho la falta de participación de las personas en la
+              oferta cultural local y la generación de identidad local,
+              disfrutamos de nuestra bella ciudad y nos propusimos desarrollar
+              una solución a estas cuestiones que nos preocupan y motivan mucho.
+            </p>
+            <p className="text-center m-2 mx-auto fw-bold fs-5 ">¡Por un Curicó, más vivo!</p>
           </Col>
           <Col md={12} lg={6}>
-            <Ratio  aspectRatio={"1x1"}>
+            <Ratio aspectRatio={"1x1"}>
               <Image
                 className="imgSecciones p-2 m-2 rounded-4"
                 src={require("./../../images/teatro.png")}
@@ -104,7 +112,7 @@ const Inicio = () => {
             </h1>
           </Col>
         </Row>
-        <Row  xs={2} sm={2} md={2} lg={3} xl={3} xxl={3}>
+        <Row xs={2} sm={2} md={2} lg={3} xl={3} xxl={3}>
           <Col>
             <a
               href="https://www.nagata.cl"
